ef70762b9f2b8e96906c93d1de27ec82b9ed4d13
Saniei <parham.saniei@fiserv.com>
2023-10-31 14:55:12 -0400

    solution

--- Solution.java
+++ Solution.java
@@ -1,12 +1,32 @@
-class Solution {
-    public static char mostFrequentChar(String s) {
-      
-      return;
-    }
-  
-    public static void main(String[] args) {
-      // Try out your test cases:
-      System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+  public static char mostFrequentChar(String s) {
+    if (s == null || s.isEmpty()) {
+      throw new IllegalArgumentException("Input string is empty or null.");
     }
 
+    Map<Character, Integer> charCount = new HashMap<>();
+    char mostFrequent = s.charAt(0);
+    int maxCount = 1;
+
+    for (char c : s.toCharArray()) {
+      int count = charCount.getOrDefault(c, 0) + 1;
+      charCount.put(c, count);
+
+      if (count > maxCount || (count == maxCount && s.indexOf(c) < s.indexOf(mostFrequent))) {
+        maxCount = count;
+        mostFrequent = c;
+      }
+    }
+
+    return mostFrequent;
   }
+
+  public static void main(String[] args) {
+    System.out.println(Solution.mostFrequentChar("bookeeper")); // -> 'e'
+  }
+
+}
